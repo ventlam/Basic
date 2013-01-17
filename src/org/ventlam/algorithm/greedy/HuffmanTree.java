e1e408aa044d7e52a06f1207227bf458b13c148a
ventlam <ventlcc@gmail.com>
2013-01-17 23:11:44 +0800

    build huffman tree in list and priorityqueue.

--- src/org/ventlam/algorithm/greedy/HuffmanTree.java
+++ src/org/ventlam/algorithm/greedy/HuffmanTree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class HuffmanTree implements Comparable<HuffmanTree>{
@@ -17,6 +18,7 @@ public class HuffmanTree implements Comparable<HuffmanTree>{
 	public HuffmanTree left;
 	public HuffmanTree right;
 	public double frequency;
+	PriorityQueue<HuffmanTree> huffQ;
 	/** Create a leaf node with the specified value and frequency. **/
 	public HuffmanTree(Character invalue,double freq)
 	{
@@ -35,7 +37,6 @@ public class HuffmanTree implements Comparable<HuffmanTree>{
 		this.left=left;
 		this.right=right;
 	}
-	
 	
 		private static HashMap<Character, Integer> charCount(String codestring)
 		{
@@ -56,10 +57,43 @@ public class HuffmanTree implements Comparable<HuffmanTree>{
 		 * @param freq The occur time of specified char
 		 * @return a Huffman tree containing the characters' prefix codes 
 		 */
-		public static HuffmanTree bulidTree(double[] freq)
+		public  void bulidTree()
 		{
-			PriorityQueue<HuffmanTree> huffq = new PriorityQueue<HuffmanTree>();
-			return huffq.peek();
+			HuffmanTree left,right,parent;
+			while(this.huffQ.size()>1)
+			{   
+				left=huffQ.remove();
+				right=huffQ.remove();
+				parent=new HuffmanTree(null,left.frequency+right.frequency);
+				huffQ.add(parent);
+			}
+					}
+		public void arraylistHuffman(List<HuffmanTree> HuffmanTrees)
+		{
+			while (HuffmanTrees.size() > 1)  
+		{
+				sort(HuffmanTrees);  
+	            //获取权值最小的两个节点  
+				HuffmanTree left = HuffmanTrees.get(HuffmanTrees.size() - 1);  
+				HuffmanTree right = HuffmanTrees.get(HuffmanTrees.size() - 2);  
+	            //生成新节点，新节点的权值为两个子节点的权值之和  
+				HuffmanTree parent = new HuffmanTree(null , left.frequency + right.frequency);  
+	            //让新节点作为权值最小的两个节点的父节点  
+	            parent.left = left;  
+	            parent.right = right;  
+	            //删除权值最小的两个节点  
+	            HuffmanTrees.remove(HuffmanTrees.size() - 1);  
+	            HuffmanTrees.remove(HuffmanTrees.size() - 1);  
+	            //将新生成的父节点添加到集合中  
+	            HuffmanTrees.add(parent);  
+		}
+		
+		
+		
+	    }
+		private void sort(List<HuffmanTree> huffmanTrees) {
+			// TODO Auto-generated method stub
+			
 		}
 		@Override
 		public int compareTo(HuffmanTree arg0) {
@@ -67,12 +101,11 @@ public class HuffmanTree implements Comparable<HuffmanTree>{
 			return 0;
 		}
 }
-/*public void arraylistHuffman()
-{
+	/*public void arraylistHuffman()
+	{
 	ArrayList al = new ArrayList();
 	HashMap<Character, Integer> hm =charCount("fuckyouyou");
-	 Collection<Integer> c = hm.values();
-	
+	Collection<Integer> c = hm.values();
 	Iterator it =c.iterator();
 	while(it.hasNext())
 	{
